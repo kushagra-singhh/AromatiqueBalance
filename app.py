c6fb37ae1dfb212c9335d3a1607b734486caecb7
Kushagra Singh <88451512+kushagra-singhh@users.noreply.github.com>
2024-03-08 16:55:29 +0000

    init

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html", title="Hello")
+
+@app.route("/symptoms", methods=["GET", "POST"])
+def symptoms():
+    if request.method == "POST":
+        first_name = request.form.get("fname")
+        last_name = request.form.get("lname")
+        return "Your name is "+first_name + last_name
+    return render_template("symptom.html")
+
+@app.route("/oils", methods=["GET", "POST"])
+def oils():
+    
+    return render_template("oils.html")
